390af5ba07a80e6f4808988ed24e7710f847d8d8
CodingBeagle <kasper.rie@gmail.com>
2022-09-07 19:47:24 +0200

    initialized GLFW window! :D

--- two_dee_shooter/src/main.rs
+++ two_dee_shooter/src/main.rs
@@ -1,7 +1,58 @@
+use std::ffi::{ CString, CStr };
+use std::ptr;
+use std::os::raw::c_char;
+
 use beagle_glfw::*;
+
+static WIDTH: i32 = 800;
+static HEIGHT: i32 = 600;
 
 fn main() {
     unsafe {
-        
+        if glfwInit() == 0 {
+            panic!("Failed to initialize GLFW.");
+        }
+
+        // GLFW was originally designed to create an OpenGL context, so we have to tell it not to
+        // since we'll be using Vulkan.
+        glfwWindowHint(GLFW_CLIENT_API as i32, GLFW_NO_API as i32);
+
+        // Handling resized windows takes special care.
+        // Disabled for now.
+        glfwWindowHint(GLFW_RESIZABLE as i32, GLFW_FALSE as i32);
+
+        let window_title = ffi_string("Two Dee Shooter");
+        let main_window = glfwCreateWindow(
+            WIDTH,
+            HEIGHT,
+            window_title.as_ptr(),
+            ptr::null_mut(),
+            ptr::null_mut());
+
+        // If main_window is NULL, window creation failed for some reason.
+        if main_window.is_null() {
+            panic!("Failed to create window: {}", get_latest_glfw_error_description());
+        }
+
+        while glfwWindowShouldClose(main_window) == 0 {
+            glfwPollEvents();
+        }
     }
+}
+
+/*
+    When communicating with unsafe bindings, I make use of the "CString" type: https://docs.rs/rustc-std-workspace-std/1.0.1/std/ffi/struct.CString.html
+    This type represents an owned, C-comptable, null-terminated string.
+    The important part for me right now being that it's nul-terminated, which many C APIs expect.
+*/
+fn ffi_string(str: &str) -> CString {
+    let error_message = format!("Failed to generate CString from {}", str);
+    CString::new(str).expect(&error_message)
+}
+
+unsafe fn get_latest_glfw_error_description() -> String {
+    let mut error_description_raw: *const i8 = ptr::null_mut();
+    glfwGetError(&mut error_description_raw);
+    let error_description = CString::from_raw(error_description_raw as *mut i8);
+    error_description.into_string().expect("Failed to convert GLFW error description into String type")
 }
