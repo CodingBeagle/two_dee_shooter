3b4bc1449e3535ef0a198d013b1e844688152e05
CodingBeagle <kasper.rie@gmail.com>
2022-10-04 18:41:40 +0200

    enabled debug callbacks using the DebugUtils extension

--- two_dee_shooter/src/main.rs
+++ two_dee_shooter/src/main.rs
@@ -1,4 +1,4 @@
-use std::ffi::{ CString, CStr };
+use std::ffi::{ CString, CStr, c_void };
 use std::ptr;
 use std::os::raw::c_char;
 
@@ -8,6 +8,39 @@ use beagle_glfw::*;
 
 static WIDTH: i32 = 800;
 static HEIGHT: i32 = 600;
+
+// Callback function used by Debug Utils extension.
+// TODO: What does extern "system" mean?
+unsafe extern "system" fn vulkan_debug_utils_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    p_user_data: *mut c_void) -> vk::Bool32 {
+
+        let severity = match message_severity {
+            vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "[Verbose]",
+            vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => "[Warning]",
+            vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => "[ERROR]",
+            vk::DebugUtilsMessageSeverityFlagsEXT::INFO => "[INFO]",
+            _ => "[Unknown]"
+        };
+
+        let types = match message_type {
+            vk::DebugUtilsMessageTypeFlagsEXT::GENERAL => "[General]",
+            vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE => "[Performance]",
+            vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION => "[Validation]",
+            _ => "[Unknown]"
+        };
+
+        let message = CStr::from_ptr((*p_callback_data).p_message);
+
+        println!("[Debug]{}{}{:?}", severity, types, message);
+
+        // The callback returns a boolean that indicates if the Vulkan call that triggered the validation layer message should
+        // be aborted. If the callback returns true, the call is aborted.
+        // This is normally used used to test the validation layers themselves, so you should always return VK_FALSE.
+        vk::FALSE
+}
 
 fn main() {
     unsafe {
@@ -95,6 +128,11 @@ fn main() {
             .map(|x| x.as_ptr())
             .collect();
 
+        // The Debug Utils debug messenger requires a valid instance in order to be created. In order to enable debug callbacks when creating the instance,
+        // You can instead pass a DebugUtilsMessengerCreateInfoEXT object pointer to the InstanceCreateInfo struct's p_next property.
+        // TODO: Do I need to handle the lifetime of this instance debug messenger myself??
+        let instance_debug_messenger = populate_debug_messenger_create_info();
+
         let create_info = vk::InstanceCreateInfo {
             s_type: vk::StructureType::INSTANCE_CREATE_INFO,
             p_application_info: &application_info,
@@ -102,12 +140,19 @@ fn main() {
             pp_enabled_extension_names: required_extensions_pointer.as_ptr(),
             pp_enabled_layer_names: validation_layers_as_raw_pointers.as_ptr(),
             enabled_layer_count: required_validation_layers.len() as u32,
+            p_next: &instance_debug_messenger as *const vk::DebugUtilsMessengerCreateInfoEXT as *const c_void,
             ..Default::default()
         };
 
         // Now everything is specified for Vulkan to create an instance
         // This instance should live for as long as the application lives.
         let vk_instance = entry.create_instance(&create_info, None).expect("Failed to create Vulkan instance.");
+
+        // In order to create a debug messenger, we have to call the function "vkCreateDebugUtilsMessengerEXT"
+        // Since this is an extension function, it is not automatically loaded with Vulkan.
+        // We have to load it ourselves
+        let debug_utils_loader = ash::extensions::ext::DebugUtils::new(&entry, &vk_instance);
+        let debug_utils_messenger = setup_debug_messenger(&debug_utils_loader);
 
         // GLFW was originally designed to create an OpenGL context, so we have to tell it not to
         // since we'll be using Vulkan.
@@ -133,6 +178,11 @@ fn main() {
         while glfwWindowShouldClose(main_window) == 0 {
             glfwPollEvents();
         }
+
+        // Clean up the debug messenger
+        // Destroying the debug messenger must be done before the Vulkan instance is destroyed.
+        // TODO: Does Ash handle any of these calls in Drop implementations of the structs??
+        debug_utils_loader.destroy_debug_utils_messenger(debug_utils_messenger, None);
 
         // Before we terminate the application, we destroy the Vulkan instance.
         vk_instance.destroy_instance(None);
@@ -163,6 +213,28 @@ unsafe fn build_extensions() -> Vec<String> {
     required_extensions.push(String::from("VK_EXT_debug_utils"));
 
     required_extensions
+}
+
+unsafe fn populate_debug_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
+    vk::DebugUtilsMessengerCreateInfoEXT {
+        s_type: vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+        message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+        pfn_user_callback: Some(vulkan_debug_utils_callback),
+        p_user_data: ptr::null_mut(),
+        ..Default::default()
+    }
+}
+
+unsafe fn setup_debug_messenger(debug_utils_ext: &ash::extensions::ext::DebugUtils) -> vk::DebugUtilsMessengerEXT {
+    // Fill out the struct describing the kind of debug messenger we'd like
+    let messenger_create_into = populate_debug_messenger_create_info();
+
+    let debug_utils_messenger = debug_utils_ext
+        .create_debug_utils_messenger(&messenger_create_into, None)
+        .expect("Failed to create Debug Utils Messenger");
+
+    debug_utils_messenger
 }
 
 /*
